b7432ffef7cc8a974a7b7db1e7e6bd4584adc8cc
Jesse Kramer <jesse@jessekramer.dev>
2024-12-03 22:59:42 +0100

    This is not the solution, almost there

--- 2024/day-02/part2/index.js
+++ 2024/day-02/part2/index.js
@@ -12,17 +12,34 @@ const reports = input.trim().split('\n')
                 }
                 return parsed
             });
-    })
+    });
 
-const validReports = reports.filter(report => {
-    if (isReportAscending(report)) {
-        return isReportSteadilyIncreasing(report);
-    } else {
-        return isReportSteadilyDecreasing(report)
-    }
+// const validReports = reports.filter(report => {
+//     return validateReport(report)
+// });
+
+([
+    [[7,6,4,2,1], true],
+    [[1,2,7,8,9], false],
+    [[9,7,6,2,1], false],
+    [[1,3,2,4,5], true],
+    [[8,6,4,4,1], true],
+    [[1,3,6,7,9], true],
+]).forEach(([report, expected]) => {
+    console.log({report, expected, result: validateReport(report)})
 })
 
-console.log(validReports.length)
+function validateReport(report) {
+    console.log('\n\tValidating report: ' + report.join(','))
+    if (isReportAscending(report)) {
+        console.log('\tReport is ascending')
+        return isReportSteadilyIncreasing(report);
+    } else {
+        console.log('\tReport is descending')
+        return isReportSteadilyDecreasing(report)
+    }
+}
+
 
 function isReportAscending(report) {
     // This does not cover the case where the report is neither ascending nor descending
@@ -30,20 +47,69 @@ function isReportAscending(report) {
     return report[0] < report[1];
 }
 function isReportSteadilyIncreasing(report) {
+    let isDampenerAvailable = true;
     return report.every((curr, i, arr) => {
-        if (i === 0) {
+        console.log('\t\tEvaluating ', i)
+        if (i < 2) {
+            console.log('\t\t\tSkipping as it is one of the first two')
             return true;
         }
-        const prev = arr[i-1]
-        return curr > prev && curr - prev <= 3;
+        // We will evaluate elements A,B,C
+        const a = arr[i-2]
+        const b = arr[i-1]
+        const c = curr
+        console.log('\t\t\tEvaluating ', a, b, c)
+
+        const isValid = (x,y) => y > x && (y - x) <= 3
+        if (isValid(a,b) && isValid(b,c)) {
+            console.log('\t\t\t\tValid')
+            return true
+        }
+        if (! isDampenerAvailable) {
+            console.log('\t\t\t\tInvalid, no dampener available')
+            return false
+        }
+        console.log('\t\t\t\tDampener available')
+
+        if (isValid(a,c)) {
+            console.log('\t\t\t\tIs valid, with dampener, using it.')
+            isDampenerAvailable = false
+            return true
+        }
+        console.log('\t\t\t\tInvalid, even with dampener')
+        return false
     })
 }
 function isReportSteadilyDecreasing(report) {
+    let isDampenerAvailable = true;
     return report.every((curr, i, arr) => {
-        if (i === 0) {
+        console.log('\t\tEvaluating ', i)
+        if (i < 2) {
+            console.log('\t\t\tSkipping as it is one of the first two')
             return true;
         }
-        const prev = arr[i-1]
-        return curr < prev && prev - curr <= 3;
+        // We will evaluate elements A,B,C
+        const a = arr[i-2]
+        const b = arr[i-1]
+        const c = curr
+        console.log('\t\t\tEvaluating ', a, b, c)
+        const isValid = (x,y) => y < x && (x - y) <= 3
+        if (isValid(a,b) && isValid(b,c)) {
+            console.log('\t\t\t\tValid')
+            return true
+        }
+        if (! isDampenerAvailable) {
+            console.log('\t\t\t\tInvalid, no dampener available')
+            return false
+        }
+        console.log('\t\t\t\tDampener available')
+
+        if (isValid(a,c)) {
+            console.log('\t\t\t\tIs valid, with dampener, using it.')
+            isDampenerAvailable = false
+            return true
+        }
+        console.log('\t\t\t\tInvalid, even with dampener')
+        return false
     })
 }
